0c4ef158b2eab01055952ef446796436f838d2cd
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-09-06 18:28:05 +0700

    Update
    Source Code

---
+++ KarnelTravel/KarnelTravel/Areas/Adminstrator/AdminstratorAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace KarnelTravel.Areas.Adminstrator
+{
+    public class AdminstratorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Adminstrator";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Adminstrator_default",
+                "Adminstrator/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
