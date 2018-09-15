9c9ec9a89fafdfeecc8097ddf817392fed443b0e
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-09-15 21:24:27 +0700

    Cusomer & Room

---
+++ eProjectSEM3/Karnel_Travels/Areas/Administrator/Controllers/RoomsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Karnel_Travels.Models;
+
+namespace Karnel_Travels.Areas.Administrator.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private KarnelTravelEntities db = new KarnelTravelEntities();
+
+        // GET: Administrator/Rooms
+        public ActionResult Index()
+        {
+            var rooms = db.Rooms.Include(r => r.Hotel);
+            return View(rooms.ToList());
+        }
+
+        // GET: Administrator/Rooms/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // GET: Administrator/Rooms/Create
+        public ActionResult Create()
+        {
+            ViewBag.Hotel_Id = new SelectList(db.Hotels, "Hotel_Id", "Hotel_Name");
+            return View();
+        }
+
+        // POST: Administrator/Rooms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Room_Id,Room_Type,Room_Name,Room_Start,Room_Stop,Room_Cost,Hotel_Id")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rooms.Add(room);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Hotel_Id = new SelectList(db.Hotels, "Hotel_Id", "Hotel_Name", room.Hotel_Id);
+            return View(room);
+        }
+
+        // GET: Administrator/Rooms/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Hotel_Id = new SelectList(db.Hotels, "Hotel_Id", "Hotel_Name", room.Hotel_Id);
+            return View(room);
+        }
+
+        // POST: Administrator/Rooms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Room_Id,Room_Type,Room_Name,Room_Start,Room_Stop,Room_Cost,Hotel_Id")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(room).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Hotel_Id = new SelectList(db.Hotels, "Hotel_Id", "Hotel_Name", room.Hotel_Id);
+            return View(room);
+        }
+
+        // GET: Administrator/Rooms/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Room room = db.Rooms.Find(id);
+            if (room == null)
+            {
+                return HttpNotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Administrator/Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Room room = db.Rooms.Find(id);
+            db.Rooms.Remove(room);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
