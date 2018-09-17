ae87e3342f1209e583f7ddd0ac5155d43915f056
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-09-17 23:31:05 +0700

    Abouts GUI

---
+++ eProjectSEM3/Karnel_Travels/Models/AboutMetaData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Karnel_Travels.Models
+{
+    [MetadataTypeAttribute(typeof(About.AboutMetaData))]
+
+    public partial class About
+    {
+        internal sealed class AboutMetaData
+        {
+            [Display(Name ="ID")]
+            public int AboutId { get; set; }
+
+            [Display(Name = "Full Name")]
+            public string AboutName { get; set; }
+
+            [Display(Name = "Birthday")]
+            [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+            [DataType(DataType.Date)]
+            public Nullable<System.DateTime> AboutBirth { get; set; }
+
+            [Display(Name = "Gender")]
+            public Nullable<bool> AboutGender { get; set; }
+
+            [Display(Name = "Programming")]
+            [DataType(DataType.MultilineText)]
+            public string AboutLike { get; set; }
+
+            [Display(Name = "Img")]
+            public string AboutImg { get; set; }
+        }
+    }
+}
