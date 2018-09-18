0f62e90b4241e45a9ed21455016d4c85df430998
TTPi <phongb1505902@student.ctu.edu.vn>
2018-09-18 16:07:23 +0700

    upload Foods Img(Multi)

--- eProjectSEM3/Karnel_Travels/Areas/Administrator/Controllers/FoodsController.cs
+++ eProjectSEM3/Karnel_Travels/Areas/Administrator/Controllers/FoodsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -120,6 +121,77 @@ namespace Karnel_Travels.Areas.Administrator.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult UploadFoods(string id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var products = db.Foods.Include(s => s.ImgFoods).SingleOrDefault(p => p.Food_Code == id);
+            if (products == null)
+            {
+                object Err = "Information not find";
+                return View("Error", Err);
+            }
+            return View(products);
+        }
+        [HttpPost]
+        public ActionResult UploadFoods(string id, HttpPostedFileBase[] files)
+        {
+            byte max = 0;
+            var listImg = db.ImgFoods.Where(p => p.Food_Code == id).ToList();
+            if (listImg.Count > 0)
+                max = listImg.Max(p => p.Img_Foods_Sort);
+            var listFile = files.Where(p => p != null);
+            foreach (var f in listFile)
+            {
+                //T?o m?t d?i tu?ng
+                var img = new ImgFood();
+                img.Food_Code = id;
+                img.Img_Foods = f.FileName;
+                img.Img_Foods_Sort = ++max;
+                db.ImgFoods.Add(img);
+                var path = Server.MapPath("~/myImg/Foods/" + f.FileName);
+                f.SaveAs(path);
+            }
+            if (listFile.Any())
+                db.SaveChanges();
+            return RedirectToAction("UploadFoods");
+        }
+
+        public ActionResult DeleteImg(int id, string Food_Code)
+        {
+            if (Food_Code == null)
+            {
+                try
+                {
+                    var img = db.ImgFoods.Find(id);
+                    if (img == null)
+                        return RedirectToAction("Index");
+                    db.ImgFoods.Remove(img);
+                    var fileName = img.Img_Foods;
+                    var path = Server.MapPath("~/myImg/Foods/" + fileName);
+                    var file = new FileInfo(path);
+
+                    if (file.Exists)
+                    {
+                        file.Delete();
+                    }
+
+                    db.SaveChanges();
+                    return RedirectToAction("UploadFoods");
+                }
+
+                catch (Exception ex)
+                {
+                    object mess = "Can not Delete IMG " + ex.Message;
+                    return View("Error", mess);
+                }
+            }
+
+            TempData["Success_Mess"] = "<script>alert('Delete Success')</script>";
+            return Redirect("~/Foods/UploadFoods/" + Food_Code);
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
