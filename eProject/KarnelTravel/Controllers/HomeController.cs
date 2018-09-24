c61872d7350409659c3a7c07563a3a857eead7f7
TTPi <phongb1505902@student.ctu.edu.vn>
2018-09-25 01:47:31 +0700

    Touris v1

--- eProject/KarnelTravel/Controllers/HomeController.cs
+++ eProject/KarnelTravel/Controllers/HomeController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using KarnelTravel.Models;
 namespace KarnelTravel.Controllers
 {
     public class HomeController : Controller
     {
+        KarnelTravelEntities db = new KarnelTravelEntities();
         public ActionResult Index()
         {
+            var TouristSpots1 = db.TouristSpots.Where(n => n.TouristSpot_Status == true);
+            ViewBag.TouristSpots1 = TouristSpots1;
             return View();
+        }
+        public ActionResult _TourisSpot()
+        {
+            return PartialView();
         }
     }
 }
