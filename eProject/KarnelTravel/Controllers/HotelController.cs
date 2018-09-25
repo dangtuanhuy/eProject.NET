57bae9b4269ef37ded93a568a19e33922d3529b2
TTPi <phongb1505902@student.ctu.edu.vn>
2018-09-25 11:02:02 +0700

    up Reautaurt, Hotel(user)

---
+++ eProject/KarnelTravel/Controllers/HotelController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KarnelTravel.Models;
+
+namespace KarnelTravel.Controllers
+{
+    public class HotelController : Controller
+    {
+        KarnelTravelEntities db = new KarnelTravelEntities();
+        public ActionResult Index()
+        {
+            var lstHotel1 = db.Hotels.Where(u => u.TouristSpot_Id == "TS1");
+            ViewBag.lstHotel1 = lstHotel1;
+            var lstHotel2 = db.Hotels.Where(u => u.TouristSpot_Id == "TS2");
+            ViewBag.lstHotel2 = lstHotel2;
+            return View();
+        }
+        public ActionResult _partiHotel1()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ eProject/KarnelTravel/Controllers/RestaurantController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KarnelTravel.Models;
+namespace KarnelTravel.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        KarnelTravelEntities db = new KarnelTravelEntities();
+        public ActionResult Index()
+        {
+            var lstRest1 = db.Restaurants.Where(u => u.TouristSpot_Id == "TS1");
+            ViewBag.lstRest1 = lstRest1;
+            return View();
+        }
+        public ActionResult _partiRest1()
+        {
+            return PartialView();
+        }
+    }
+}
