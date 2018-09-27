3eeb0532b8baece7344a2d8eddc47506a1fe8bf3
TTPi <phongb1505902@student.ctu.edu.vn>
2018-09-27 22:22:33 +0700

    up Giao dien, (err Paglist)

--- eProject/KarnelTravel/Controllers/RestaurantController.cs
+++ eProject/KarnelTravel/Controllers/RestaurantController.cs
@@ -4,16 +4,42 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using KarnelTravel.Models;
+using PagedList;
+
 namespace KarnelTravel.Controllers
 {
     public class RestaurantController : Controller
     {
         KarnelTravelEntities db = new KarnelTravelEntities();
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string searchString, int? page, string currentFilter)
         {
             var lstRest1 = db.Restaurants.Where(u => u.TouristSpot_Id == "TS1");
             ViewBag.lstRest1 = lstRest1;
-            return View();
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+            var lstNew = from s in db.Restaurants
+                         select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                lstNew = lstNew.Where(s => s.Restaurant_Name.Contains(searchString)
+                                       || s.Restaurant_Address.Contains(searchString));
+            }
+
+            lstNew = lstNew.OrderBy(s => s.Restaurant_Code);
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(lstNew.ToPagedList(pageNumber, pageSize));
         }
         public ActionResult _partiRest1()
         {
---
+++ eProject/KarnelTravel/Controllers/TourisSpotController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KarnelTravel.Controllers
+{
+    public class TourisSpotController : Controller
+    {
+        // GET: TourisSpot
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult _ImgTravel()
+        {
+            return PartialView();
+        }
+    }
+}
