16835e53803018bf5eae1ef02ff627290fd8e80a
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-09-28 18:35:23 +0700

    CSDL

---
+++ eProject/KarnelTravel/Areas/Management/Controllers/PaymentMethodsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KarnelTravel.Models;
+
+namespace KarnelTravel.Areas.Management.Controllers
+{
+    public class PaymentMethodsController : Controller
+    {
+        private KarnelTravelEntities db = new KarnelTravelEntities();
+
+        // GET: Management/PaymentMethods
+        public ActionResult Index()
+        {
+            return View(db.PaymentMethods.ToList());
+        }
+
+        // GET: Management/PaymentMethods/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PaymentMethod paymentMethod = db.PaymentMethods.Find(id);
+            if (paymentMethod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paymentMethod);
+        }
+
+        // GET: Management/PaymentMethods/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Management/PaymentMethods/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PaymentId,PaymentName")] PaymentMethod paymentMethod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PaymentMethods.Add(paymentMethod);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(paymentMethod);
+        }
+
+        // GET: Management/PaymentMethods/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PaymentMethod paymentMethod = db.PaymentMethods.Find(id);
+            if (paymentMethod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paymentMethod);
+        }
+
+        // POST: Management/PaymentMethods/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PaymentId,PaymentName")] PaymentMethod paymentMethod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paymentMethod).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(paymentMethod);
+        }
+
+        // GET: Management/PaymentMethods/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PaymentMethod paymentMethod = db.PaymentMethods.Find(id);
+            if (paymentMethod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paymentMethod);
+        }
+
+        // POST: Management/PaymentMethods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PaymentMethod paymentMethod = db.PaymentMethods.Find(id);
+            db.PaymentMethods.Remove(paymentMethod);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
