d3bf6686c1d759d8d71ecd68731b4cd14619124f
TTPi <phongb1505902@student.ctu.edu.vn>
2018-09-30 01:03:02 +0700

    Code display Home

--- eProject/KarnelTravel/Controllers/HomeController.cs
+++ eProject/KarnelTravel/Controllers/HomeController.cs
@@ -11,8 +11,11 @@ namespace KarnelTravel.Controllers
         KarnelTravelEntities db = new KarnelTravelEntities();
         public ActionResult Index()
         {
-            var TouristSpots1 = db.TouristSpots.Where(u => u.Location_Id == "LC1");
+            var TouristSpots1 = db.TouristSpots.Where(u => u.TouristSpot_Status == true).OrderByDescending(u => u.TouristSpot_Create);
             ViewBag.TouristSpots1 = TouristSpots1;
+
+            var Restaurant1 = db.Restaurants.Where(n => n.Restaurant_Status == true).OrderByDescending(n => n.Restaurant_Create);
+            ViewBag.Restaurant1 = Restaurant1;
             return View();
         }
         public ActionResult _TourisSpot()
