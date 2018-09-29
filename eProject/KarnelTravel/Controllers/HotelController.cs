e502fa21ac2beeee47656f5cd8eb38975ab28db6
Hoàng Sơn <pnhson16074@cusc.ctu.edu.vn>
2018-09-29 13:27:27 +0700

    Update layout hotel

--- eProject/KarnelTravel/Controllers/HotelController.cs
+++ eProject/KarnelTravel/Controllers/HotelController.cs
@@ -14,7 +14,7 @@ namespace KarnelTravel.Controllers
         KarnelTravelEntities db = new KarnelTravelEntities();
         public ActionResult Index(string sortOrder, string searchString, int? page, string currentFilter)
         {
-            var lstHotel1 = db.Hotels.Where(u => u.TouristSpot_Id == "TS1");
+            var lstHotel1 = db.Hotels.Where(u => u.Hotel_Status == true);
             ViewBag.CurrentSort = sortOrder;
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             if (searchString != null)
@@ -31,7 +31,7 @@ namespace KarnelTravel.Controllers
                 lstHotel1 = lstHotel1.Where(s => s.Hotel_Name.Contains(searchString)
                                        || s.Hotel_Address.Contains(searchString));
             }
-            int PageSize = 6;
+            int PageSize = 9;
             int PageNumber = (page ?? 1);
             return View(lstHotel1.OrderBy(n => n.Hotel_Id).ToPagedList(PageNumber, PageSize));
         }
