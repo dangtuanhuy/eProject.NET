956751f0a605dafe942de2748679e0405e6a2851
Hoàng Sơn <pnhson16074@cusc.ctu.edu.vn>
2018-09-29 10:13:26 +0700

    Update Image + check Valid

--- eProject/KarnelTravel/Models/LocationMetaData.cs
+++ eProject/KarnelTravel/Models/LocationMetaData.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace KarnelTravel.Models
 {
-    public class LocationMetaData
+    [MetadataType(typeof(Location.LocationMetaData))]
+    public partial class Location
     {
-
+        internal sealed class LocationMetaData
+        {
+            [Required]
+            [DisplayName("Location ID")]
+            [Remote("CheckUserName", controller: "Users", ErrorMessage = "Tên đăng nhập đã tồn tại")]
+            public string Location_Id { get; set; }
+            [Required]
+            [DisplayName("Location Name")]
+            public string Location_Name { get; set; }
+            [Required]
+            [DisplayName("Location Location")]
+            public string Location_Location { get; set; }
+        }
     }
 }
