78b1fed94ccf8ae7ee86fcd7c4b587ece9518e2a
TTPi <phongb1505902@student.ctu.edu.vn>
2018-10-04 17:22:50 +0700

    GioHang(tets)

---
+++ eProject/KarnelTravel/Controllers/CartController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KarnelTravel.Models;
+using Microsoft.AspNet.Identity;
+namespace KarnelTravel.Controllers
+{
+    public class CartController : Controller
+    {
+        KarnelTravelEntities db = new KarnelTravelEntities();
+        public List<ItemGioHang> LayGioHang()
+        {
+            // Giỏ hàng đã tồn tại
+            List<ItemGioHang> lstGioHang = Session["GioHang"] as List<ItemGioHang>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<ItemGioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+
+        //Thêm giỏ hàng thông thường load lại trang
+
+        public ActionResult ThemGioHang(String TouristSpotId, String strUrl)
+        {
+            // Kiểm tra sản phẩm có tồn tại trong CSLD hay không?
+            TouristSpot sp = db.TouristSpots.SingleOrDefault(n => n.TouristSpot_Id == TouristSpotId);
+            if (sp == null)
+            {
+                // Trang đường dẫn không hợp lệ
+                Response.StatusCode = 404;
+            }
+            // Lấy giỏ hàng
+            List<ItemGioHang> lstGioHang = LayGioHang();
+
+            //TH1 sản phẩm đã tồn tại trong giỏ hàng
+            ItemGioHang spCheck = lstGioHang.SingleOrDefault(n => n.TouristSpot_Id == TouristSpotId);
+            if (spCheck != null)
+            {
+                if (sp.TouristSpot_Status == false)
+                {
+                    return View("ThongBao");
+                }
+                spCheck.TourisQty++;
+                spCheck.ThanhTien = spCheck.TourisQty * spCheck.TouristSpot_Price;
+                return Redirect(strUrl);
+            }
+            if (sp.TouristSpot_Status == false)
+            {
+                return View("ThongBao");
+            }
+            ItemGioHang itemGH = new ItemGioHang(TouristSpotId);
+            lstGioHang.Add(itemGH);
+            return Redirect(strUrl);
+
+        }
+
+        public double TinhTongSoLuong()
+        {
+            List<ItemGioHang> lstGioHang = Session["GioHang"] as List<ItemGioHang>;
+            if (lstGioHang == null)
+            {
+                return 0;
+            }
+            return lstGioHang.Sum(n => n.TourisQty);
+        }
+        public decimal TinhTongTien()
+        {
+            List<ItemGioHang> lstGioHang = Session["GioHang"] as List<ItemGioHang>;
+            if (lstGioHang == null)
+            {
+                return 0;
+            }
+            return lstGioHang.Sum(n => n.ThanhTien);
+
+        }
+
+        // GET: ShoppingCart
+        public ActionResult XemGioHang()
+        {
+            List<ItemGioHang> giohang = Session["GioHang"] as List<ItemGioHang>;
+            return View(giohang);
+        }
+
+        public ActionResult SuaSoLuong(String TouristSpotId, int soluongmoi)
+        {
+            // tìm carditem muon sua
+            List<ItemGioHang> giohang = Session["GioHang"] as List<ItemGioHang>;
+            ItemGioHang itemSua = giohang.FirstOrDefault(m => m.TouristSpot_Id == TouristSpotId);
+            if (itemSua != null)
+            {
+                itemSua.TourisQty = soluongmoi;
+                itemSua.ThanhTien = itemSua.TourisQty * itemSua.TouristSpot_Price;
+            }
+            return RedirectToAction("XemGioHang");
+
+        }
+        public RedirectToRouteResult XoaKhoiGio(String TouristSpotId)
+        {
+            List<ItemGioHang> giohang = Session["GioHang"] as List<ItemGioHang>;
+            ItemGioHang itemXoa = giohang.FirstOrDefault(m => m.TouristSpot_Id == TouristSpotId);
+            if (itemXoa != null)
+            {
+                giohang.Remove(itemXoa);
+            }
+            return RedirectToAction("XemGioHang");
+        }
+        public ActionResult ConfirmCheckOut()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ConfirmCheckOut([Bind(Include = "Trip_Address,Trip_Details")] Trip order)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = User.Identity.GetUserName();
+                //Order order = new Order();
+                order.Trip_Date = DateTime.Now;
+                order.Customer_Id = userId;
+                //order.UserId = null;
+                order.Trip_Status = false;
+                db.Trips.Add(order);
+                db.SaveChanges();
+                // Them chi tiet
+                List<ItemGioHang> lstGH = LayGioHang();
+                foreach (var item in lstGH)
+                {
+                    var details = new TripDetail();
+                    details.Trip_Id = order.Trip_Id;
+                    details.TouristSpot_Id = item.TouristSpot_Id;
+                    details.Trip_Quantity = item.TourisQty;
+                    details.OrginalPrice = item.TouristSpot_Price;
+                    db.TripDetails.Add(details);
+
+                }
+                db.SaveChanges();
+                Session["GioHang"] = null;
+                return RedirectToAction("XemGioHang", "Cart");
+            }
+            return View();
+        }
+    }
+}
---
+++ eProject/KarnelTravel/Models/ItemGioHang.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KarnelTravel.Models;
+
+namespace KarnelTravel.Models
+{
+    public class ItemGioHang
+    {
+        public TouristSpot touristSpot { set; get; }
+        public ImgTouristSpot ImgTouristSpot { set; get; }
+        public int TourisQty { set; get; }
+        public String TouristSpot_Id { set; get; }
+        public decimal TouristSpot_Price { get; set; }
+        public List<ImgTouristSpot> ImgTouris { get; set; }
+        public String TouristSpot_Name { get; set; }
+        public Decimal ThanhTien { get; set; }
+        public ItemGioHang(String TouristSpotId)
+        {
+            using (KarnelTravelEntities db = new KarnelTravelEntities())
+            {
+                TouristSpot tour = db.TouristSpots.Include("ImgTouristSpots").Single(x => x.TouristSpot_Id == TouristSpotId);
+                this.TouristSpot_Id = TouristSpotId;
+                this.TourisQty = 1;
+                this.TouristSpot_Name = tour.TouristSpot_Name;
+                this.TouristSpot_Price = tour.TouristSpot_Price.Value;
+                this.ThanhTien = tour.TouristSpot_Price.Value * this.TourisQty;
+                this.ImgTouris = tour.ImgTouristSpots.ToList();
+
+            }
+        }
+    }
+}
