e399d0223782452383c6181bfdc0a6f749f6ab62
TTPi <phongb1505902@student.ctu.edu.vn>
2018-10-04 10:02:24 +0700

    loginUser

---
+++ eProject/KarnelTravel/Controllers/LoginUserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KarnelTravel.Models;
+
+namespace KarnelTravel.Controllers
+{
+    public class LoginUserController : Controller
+    {
+        public static bool CheckRegisterU(string username, string password)
+        {
+            var encrpytedPassword = Encrypt.MD5_Encode(password);
+            using (KarnelTravelEntities db = new KarnelTravelEntities())
+            {
+                var singin = from p in db.Customers
+                             where p.Customer_Id == username && p.Customer_Password == encrpytedPassword
+                             select p;
+                return singin.Any();
+            }
+        }
+        public ActionResult LoginU()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginU(LoginU model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = CheckRegisterU(model.Customer_Id, model.Customer_Password);
+                if (result)
+                {
+                    Session["username"] = model.Customer_Id;
+                    return RedirectToAction("Index","Home"); 
+                }
+                else
+                {
+                    return RedirectToAction("LoginU");
+                }
+            }
+            return View("LoginU");
+        }
+        public ActionResult LogOff()
+        {
+            Session["username"] = null;
+            return View();
+        }
+    }
+}
---
+++ eProject/KarnelTravel/Models/LoginU.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KarnelTravel.Models
+{
+    public class LoginU
+    {
+        [Required(ErrorMessage = "Username can not null")]
+        public string Customer_Id { get; set; }
+        [Required(ErrorMessage = "Password can not null")]
+        public string Customer_Password { get; set; }
+    }
+}
