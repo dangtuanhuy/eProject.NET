96d11afcb7cbca62cd73926670299d36a3cdd29b
Hoàng Sơn <pnhson16074@cusc.ctu.edu.vn>
2018-10-06 14:13:27 +0700

    Update Trip + About

---
+++ eProject/KarnelTravel/Areas/Management/Controllers/TripsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KarnelTravel.Models;
+
+namespace KarnelTravel.Areas.Management.Controllers
+{
+    public class TripsController : Controller
+    {
+        private KarnelTravelEntities db = new KarnelTravelEntities();
+
+        // GET: Management/Trips
+        public ActionResult Index()
+        {
+            var trips = db.Trips.Include(t => t.Customer).Include(t => t.PaymentMethod);
+            return View(trips.ToList());
+        }
+
+        // GET: Management/Trips/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // GET: Management/Trips/Create
+        public ActionResult Create()
+        {
+            ViewBag.Customer_Id = new SelectList(db.Customers, "Customer_Id", "Customer_Password");
+            ViewBag.PaymentId = new SelectList(db.PaymentMethods, "PaymentId", "PaymentName");
+            return View();
+        }
+
+        // POST: Management/Trips/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Trip_Id,Trip_Name,Trip_Details,Trip_Date,Trip_Address,Trip_Status,Customer_Id,PaymentId,MasterOnline")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trips.Add(trip);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Customer_Id = new SelectList(db.Customers, "Customer_Id", "Customer_Password", trip.Customer_Id);
+            ViewBag.PaymentId = new SelectList(db.PaymentMethods, "PaymentId", "PaymentName", trip.PaymentId);
+            return View(trip);
+        }
+
+        // GET: Management/Trips/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Customer_Id = new SelectList(db.Customers, "Customer_Id", "Customer_Password", trip.Customer_Id);
+            ViewBag.PaymentId = new SelectList(db.PaymentMethods, "PaymentId", "PaymentName", trip.PaymentId);
+            return View(trip);
+        }
+
+        // POST: Management/Trips/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Trip_Id,Trip_Name,Trip_Details,Trip_Date,Trip_Address,Trip_Status,Customer_Id,PaymentId,MasterOnline")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trip).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Customer_Id = new SelectList(db.Customers, "Customer_Id", "Customer_Password", trip.Customer_Id);
+            ViewBag.PaymentId = new SelectList(db.PaymentMethods, "PaymentId", "PaymentName", trip.PaymentId);
+            return View(trip);
+        }
+
+        // GET: Management/Trips/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // POST: Management/Trips/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trip trip = db.Trips.Find(id);
+            db.Trips.Remove(trip);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UpdateStatus(int id)
+        {
+            try
+            {
+                Trip ab = db.Trips.Where(c => c.Trip_Id == id).FirstOrDefault();
+
+                if (ab.Trip_Status == false)
+                {
+
+                    ab.Trip_Status = true;
+                }
+                else if (ab.Trip_Status == true)
+                {
+                    ab.Trip_Status = false;
+                }
+                db.SaveChanges();
+
+                return Json(ab, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        public ActionResult IndexStatus()
+        {
+            List<Trip> listAbout = new List<Trip>();
+            try
+            {
+                listAbout = db.Trips.Where(u => u.Trip_Status == false).ToList();
+
+            }
+            catch
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
+            }
+            return View(listAbout);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- eProject/KarnelTravel/Controllers/AboutController.cs
+++ eProject/KarnelTravel/Controllers/AboutController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KarnelTravel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,13 @@ namespace KarnelTravel.Controllers
 {
     public class AboutController : Controller
     {
+        KarnelTravelEntities db = new KarnelTravelEntities();
         // GET: About
         public ActionResult About()
         {
-            return View();
+            var Persion = (from p in db.Abouts select p);
+            
+            return View(Persion.ToList());
         }
     }
 }
